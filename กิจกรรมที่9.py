d64bd4b9418d04a842b994b4f3369fc2d217c6a6
napatsorn40 <161224642+napatsorn40@users.noreply.github.com>
2024-02-25 21:25:53 +0700

    Add files via upload

---
+++ กิจกรรมที่9.py
@@ -0,0 +1,23 @@
+
+result1 = ""
+for i in range(1, 16, 2):
+    result1 += str(i) + ", "
+print(result1)
+
+
+result2 = ""
+for i in range(2, 20, 3):
+    result2 += str(i) + ", "
+print(result2)
+
+
+result3 = ""
+for i in range(30, -31, -10):
+    result3 += str(i) + ", "
+print(result3)
+
+
+result4 = ""
+for i in range(15, 56, 8):
+    result4 += str(i) + ", "
+print(result4)
